603a7cd280d6d8c566c38096c026414186cc1dfa
DaMo Wang <andzven@gmail.com>
2025-01-08 18:50:33 +0800

    feat: add MaterialRequest && MaterialRequestItem

---
+++ lib/handan/production/projections/material_request.ex
@@ -0,0 +1,18 @@
+defmodule Handan.Production.Projections.MaterialRequest do
+  @moduledoc false
+
+  use Ecto.Schema
+
+  @primary_key {:uuid, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "material_requests" do
+    field :title, :string
+    field :status, :string
+
+    belongs_to :production_plan, Handan.Production.Projections.ProductionPlan, foreign_key: :production_plan_uuid, references: :uuid
+
+    has_many :items, Handan.Production.Projections.MaterialRequestItem, foreign_key: :material_request_uuid
+
+    timestamps(type: :utc_datetime)
+  end
+end
---
+++ lib/handan/production/projections/material_request_item.ex
@@ -0,0 +1,22 @@
+defmodule Handan.Production.Projections.MaterialRequestItem do
+  @moduledoc false
+
+  use Ecto.Schema
+
+  @primary_key {:uuid, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "material_request_items" do
+    field :actual_qty, :decimal
+    field :item_name, :string
+    field :umo_name, :string
+
+    belongs_to :material_request, Handan.Production.Projections.MaterialRequest, foreign_key: :material_request_uuid, references: :uuid
+    belongs_to :bom, Handan.Production.Projections.BOM, foreign_key: :bom_uuid, references: :uuid
+    belongs_to :item, Handan.Stock.Projections.Item, foreign_key: :item_uuid, references: :uuid
+    belongs_to :stock_uom, Handan.Stock.Projections.StockUOM, foreign_key: :stock_uom_uuid, references: :uuid
+    belongs_to :from_warehouse, Handan.Enterprise.Projections.Warehouse, foreign_key: :from_warehouse_uuid, references: :uuid
+    belongs_to :to_warehouse, Handan.Enterprise.Projections.Warehouse, foreign_key: :to_warehouse_uuid, references: :uuid
+
+    timestamps(type: :utc_datetime)
+  end
+end
